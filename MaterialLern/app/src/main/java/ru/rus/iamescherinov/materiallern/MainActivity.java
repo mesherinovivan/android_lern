782aaefbd754033180ef9e3612d88a81a492fa7f
Ivan Mesherinov <mesherinovivan@mail.ru>
2019-03-12 18:00:24 +0500

    Start Project 1

---
+++ MaterialLern/app/src/main/java/ru/rus/iamescherinov/materiallern/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.rus.iamescherinov.materiallern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
